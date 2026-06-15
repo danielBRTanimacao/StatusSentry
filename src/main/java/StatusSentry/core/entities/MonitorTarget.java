9a75122c380d2632b1d37f222b0fd522059224c5
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-06-15 18:10:49 -0300

    feat base function

--- src/main/java/StatusSentry/core/entities/MonitorTarget.java
+++ src/main/java/StatusSentry/core/entities/MonitorTarget.java
@@ -1,4 +1,24 @@
 package StatusSentry.core.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class MonitorTarget {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String url;
+    private Integer checkInterval;
+    private boolean isUp;
+    private LocalDateTime lastCheck;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
