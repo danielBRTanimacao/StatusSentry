cd72f5a038c259fe8ce588713696769843ae8ad8
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-06-18 09:56:54 -0300

    feat creating max config on user and Monitor

--- src/main/java/StatusSentry/core/entities/MonitorTargetEntity.java
+++ src/main/java/StatusSentry/core/entities/MonitorTargetEntity.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class MonitorTargetEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
--- src/main/java/StatusSentry/core/entities/UserEntity.java
+++ src/main/java/StatusSentry/core/entities/UserEntity.java
@@ -3,15 +3,39 @@ package StatusSentry.core.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "id", updatable = false, nullable = false, columnDefinition = "UUID")
     private UUID id;
+
+    @Column(unique = true)
+    private String username;
+    private String email;
+
+    private String verificationCode;
+    private boolean verified;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    protected boolean isExpired() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime limit = this.createdAt.plusMinutes(15L);
+
+        return now.isAfter(limit);
+    }
 }
