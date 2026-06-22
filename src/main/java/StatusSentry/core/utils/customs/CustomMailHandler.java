080a6f0a183b8f6fd806be2ad75e29e27e05f24b
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-06-22 13:58:08 -0300

    feat creating mail basic senderP

--- src/main/java/StatusSentry/core/utils/customs/CustomMailHandler.java
+++ src/main/java/StatusSentry/core/utils/customs/CustomMailHandler.java
@@ -2,26 +2,34 @@ package StatusSentry.core.utils.customs;
 
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 
 @RequiredArgsConstructor
+@Slf4j
 public class CustomMailHandler {
 
     @Value("${spring.standard.email}")
-    private static final String EMAIL;
+    private String STANDARD_EMAIL;
     private String destination;
-    private String summary;
+    
+    private String token;
 
     public void sendEmail() {
+        String SUMMARY = "Este vai ser o texto enviado para o email do caba junto do token " + this.token;
+
+        log.info("Sending the email");
+
+        log.debug("Email sent successfully");
+    }
+
+    public void setEmail(String mail) {
+        this.destination = mail;
 
     }
 
-    public void setEmail() {
-
-    }
-
-    public void getEmail() {
-
+    public String getEmail() {
+        return this.destination;
     }
 }
 
